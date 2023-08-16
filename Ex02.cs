1d3987d1e35ee7545a8e7bb054ddecf587394be4
BrunoLgN <142221480+BrunoLgN@users.noreply.github.com>
2023-08-16 19:12:54 -0300

    ex002

---
+++ Ex02.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex002;
+
+class Ex02
+{
+    static void Main(string[] args)
+    {
+        string[] nome = new string[5];
+        int cont = 0;
+
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Informe nome do animal: ");
+            nome[i] = Console.ReadLine();
+        }
+        Console.WriteLine("Exibindo matriz: ");
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(nome[i]);
+            if (nome[i].StartsWith("C", StringComparison.OrdinalIgnoreCase))
+            {
+                cont++;
+            }
+        }
+        Console.WriteLine("Numero de animais que comecam com a letra C: {0}", cont);
+    }
+}
