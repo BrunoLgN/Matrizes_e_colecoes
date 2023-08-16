6daf69a3d372bd79b159b96b340e6f960ec87336
BrunoLgN <142221480+BrunoLgN@users.noreply.github.com>
2023-08-16 19:14:10 -0300

    ex004

---
+++ Ex04.cs
@@ -0,0 +1,22 @@
+﻿namespace ex004;
+
+class Ex04
+{
+    static void Main(string[] args)
+    {
+        string[] pais = new string[5];
+
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Informe nome do país: ");
+            pais[i] = Console.ReadLine();
+        }
+        Console.WriteLine("Países ordenados na ordem alfabetica inversa: ");
+        Array.Sort(pais, (a, b) => b.CompareTo(a));
+
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("{0}", pais[i]);
+        }
+    }
+}
