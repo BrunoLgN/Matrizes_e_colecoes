3a744edcfd8492317eeb81cb9f652d27bebe8fb7
BrunoLgN <142221480+BrunoLgN@users.noreply.github.com>
2023-08-16 19:14:51 -0300

    ex006

---
+++ Ex06.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex006;
+
+class Ex06
+{
+    static void Main(string[] args)
+    {
+        List<string> cidades = new List<string>();
+        for(int i = 0; i < 5; i++)
+        {
+            Console.Write("Informe cidade: ");
+            string cidade = Console.ReadLine();
+            cidades.Add(cidade);
+        }
+
+        cidades.RemoveAll(cidade => cidade.StartsWith("S", StringComparison.OrdinalIgnoreCase));
+
+        foreach(string cidade in cidades)
+        {
+            Console.WriteLine(cidade);
+        }
+    }
+}
