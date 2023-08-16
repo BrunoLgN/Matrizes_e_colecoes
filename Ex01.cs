6ac10409ee5b686313901b18240c2fd78978592d
BrunoLgN <142221480+BrunoLgN@users.noreply.github.com>
2023-08-16 19:12:34 -0300

    ex001

---
+++ Ex01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex001;
+
+class Ex01
+{
+    static void Main(string[] args)
+    {
+        int[] inteiros = new int[5];
+        int cont = 0;
+
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Informe inteiro: ");
+            inteiros[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Inteiros informados: ");
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("{0}", inteiros[i]);
+            if((inteiros[i] % 2) == 0)
+            {
+                cont++;
+            }
+        }
+        Console.WriteLine("{0} numeros pares.", cont);
+    }
+}
