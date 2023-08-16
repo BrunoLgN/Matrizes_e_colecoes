b966e67024d14d5fbd68229c53ef5f6d52a03e04
BrunoLgN <142221480+BrunoLgN@users.noreply.github.com>
2023-08-16 19:16:19 -0300

    ex008

---
+++ Ex08.cs
@@ -0,0 +1,24 @@
+﻿namespace ex008;
+
+class Ex08
+{
+    static void Main(string[] args)
+    {
+        string[] paises = new string[5];
+        int cont = 0;
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Informe nome do pais: ");
+            paises[i] = Console.ReadLine();
+        }
+
+        foreach(string pais in paises)
+        {
+            if(pais.Length > 10)
+            {
+                cont++;
+            }
+        }
+        Console.WriteLine("Paises com mais de 10 letras: {0}", cont);
+    }
+}
