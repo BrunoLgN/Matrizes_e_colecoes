568096934f492b06cd4ee1764f790d33378cbb52
BrunoLgN <142221480+BrunoLgN@users.noreply.github.com>
2023-08-16 19:14:32 -0300

    Add files via upload

---
+++ Ex05.cs
@@ -0,0 +1,21 @@
+﻿namespace ex005;
+
+class Ex05
+{
+    static void Main(string[] args)
+    {
+        int[] matriz = new int[5];
+        int soma = 0;
+
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Informe um inteiro: ");
+            matriz[i] = int.Parse(Console.ReadLine());
+        }
+        for(int i = 0; i < 5; i++)
+        {
+            soma += matriz[i];
+        }
+        Console.WriteLine("Soma de todos os numeros da matriz: {0}", soma);
+    }
+}
