d34f3733e2059c7c2149c6b74a7fc32374453e64
BrunoLgN <142221480+BrunoLgN@users.noreply.github.com>
2023-08-16 19:16:41 -0300

    ex009

---
+++ Ex09.cs
@@ -0,0 +1,25 @@
+﻿namespace ex009;
+
+class Ex09
+{
+    static void Main(string[] args)
+    {
+        string[] cores = new string[5];
+        int cont = 0;
+
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Informe cor: ");
+            cores[i] = Console.ReadLine();
+        }
+
+        foreach(string cor in cores)
+        {
+            if(cor == "vermelho" || cor == "amarelo" || cor == "verde")
+            {
+                cont++;
+            }
+        }
+        Console.WriteLine("Numero de cores primarias: {0}", cont);
+    }
+}
