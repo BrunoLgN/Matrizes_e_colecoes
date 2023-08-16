cec88896a0c952ebc2000d95415fbcbad8592edf
BrunoLgN <142221480+BrunoLgN@users.noreply.github.com>
2023-08-16 19:13:49 -0300

    ex003

---
+++ Ex03.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+namespace ex003;
+
+class Ex03
+{
+    static void Main(string[] args)
+    {
+        
+        string[] frutas = new string[5];
+
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Informe fruta: ");
+            frutas[i] = Console.ReadLine();
+        }
+        Array.Sort(frutas);
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("{0}", frutas[i]);
+        }
+    }
+}
