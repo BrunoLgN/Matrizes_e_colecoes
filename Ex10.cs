b0c37b2099ceb82decb5a206a3594912a24df960
BrunoLgN <142221480+BrunoLgN@users.noreply.github.com>
2023-08-16 19:17:09 -0300

    ex010

---
+++ Ex10.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex010;
+
+class Ex10
+{
+    static void Main(string[] args)
+    {
+        string[] frutas = new string[5];
+
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Informe fruta: ");
+            frutas[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine("Primeira fruta: {0}", frutas[0]);
+        Console.WriteLine("Ultima fruta: {0}", frutas[4]);
+    }
+}
